949c8663f24a15fb10630fc91ccb159c2a358691
Haitore <ender12as@gmail.com>
2023-04-23 12:22:14 -0600

    Implementacion de servicio (Comprobar funcionamiento)

--- src/app/tmdb.service.ts
+++ src/app/tmdb.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Movie } from './movie';
 import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TMDBService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  getMovie(id: number): Observable<Movie> | void {}
+  getMovie(id: number): Observable<Movie> | void {
+    const url = 'https://dev.ernestorb.com/movies/API/Movies/' + id;
+    return this.http.get<Movie>(url);
+  }
 
-  searchMovie(search: string): Observable<Movie> | void {}
+  searchMovie(search: string): Observable<Movie> | void {
+    const url = 'https://dev.ernestorb.com/movies/API/Movies';
+    let params = {"search": search, "page": "2"};
+    let query = new HttpParams({ fromObject: params });
+    return this.http.get<Movie>(url, {params: query});
+  }
 
-  nowMovies(): Observable<Movie[]> | void {}
+  nowMovies(): Observable<Movie[]> | void {
+    const url = 'https://dev.ernestorb.com/movies/API/Movies';
+    return this.http.get<Movie[]>(url);
+  }
 }
