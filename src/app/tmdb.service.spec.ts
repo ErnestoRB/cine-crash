b3fca7dc9b2a53c6b65554cc813025aaf5cbcd32
Ernesto Ramírez <ernestorb_@outlook.com>
2023-04-27 20:17:17 -0600

    Ejemplo de transformación de datos

--- src/app/tmdb.service.spec.ts
+++ src/app/tmdb.service.spec.ts
@@ -1,16 +1,45 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TMDBService } from './tmdb.service';
+import { HttpClient } from '@angular/common/http';
+import { from, of } from 'rxjs';
+import { PaginationResponse } from './pagination-response';
+import { Movie } from './movie';
 
 describe('TMDBService', () => {
   let service: TMDBService;
+  let spy: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    spy = jasmine.createSpyObj('ValueService', ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: spy,
+        },
+      ],
+    });
     service = TestBed.inject(TMDBService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('nowMovies() should return the transformed response', () => {
+    const observable = of<PaginationResponse<Movie>>({
+      page: 1,
+      results: [],
+      total_results: 0,
+      total_pages: 0,
+    });
+    spy.get.and.returnValue(observable);
+
+    service.nowMovies().subscribe((movies) => {
+      expect(spy.get).toHaveBeenCalledTimes(1);
+      expect(movies).toEqual([]);
+    });
   });
 });
--- src/app/tmdb.service.ts
+++ src/app/tmdb.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Movie } from './movie';
-import { Observable } from 'rxjs';
+import { Observable, map, pipe } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { PaginationResponse } from './pagination-response';
 
 @Injectable({
   providedIn: 'root',
@@ -9,24 +10,32 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 export class TMDBService {
   constructor(private http: HttpClient) {}
 
-  getMovie(id: string): Observable<Movie> | void {
+  getMovie(id: string): Observable<Movie> {
     const url = 'https://dev.ernestorb.com/tmdb/find/' + id;
-    let params = {"external_source": "imdb_id", "page": "1"};
+    let params = { external_source: 'imdb_id', page: '1' };
     let query = new HttpParams({ fromObject: params });
-    return this.http.get<Movie>(url);
+    return this.http.get<Movie>(url, { params: query });
   }
 
-  searchMovie(search: string, page: number = 1): Observable<Movie> | void {
+  searchMovies(
+    search: string,
+    page: number = 1
+  ): Observable<PaginationResponse<Movie>> {
     const url = 'https://dev.ernestorb.com/tmdb/search/movie';
-    let params = {"query": search, "page": page};
+    let params = { query: search, page: page };
     let query = new HttpParams({ fromObject: params });
-    return this.http.get<Movie>(url, {params: query});
+    return this.http.get<PaginationResponse<Movie>>(url, { params: query });
   }
 
-  nowMovies(page: number = 1): Observable<Movie[]> | void {
+  nowMovies(page: number = 1): Observable<Movie[]> {
     const url = 'https://dev.ernestorb.com/tmdb/movie/now_playing';
-    let params = {"page": page};
+    let params = { page: page };
     let query = new HttpParams({ fromObject: params });
-    return this.http.get<Movie[]>(url, {params: query});
+    const transformResponse = pipe(
+      map((movie: PaginationResponse<Movie>) => movie.results)
+    );
+    return transformResponse(
+      this.http.get<PaginationResponse<Movie>>(url, { params: query })
+    );
   }
 }
