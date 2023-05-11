16479277b228bc52ab6363a55f78c4c1230f7dfb
Ernesto Ramírez <ernestorb_@outlook.com>
2023-05-10 21:57:02 -0600

    Botón con mejor área de click (SearchComponent)

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,22 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Movie } from '@models';
-import { TMDBService } from '@services';
-
-
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.sass']
+  styleUrls: ['./search.component.sass'],
 })
 export class SearchComponent implements OnInit {
+  movieToSearch?: string;
 
-  movieToSearch !: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
