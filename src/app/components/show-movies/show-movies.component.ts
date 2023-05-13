a18113f74c16ff023e7b8f2828d2f10fb71f5823
Ernesto Ramírez <ernestorb_@outlook.com>
2023-05-12 22:54:05 -0600

    Mejorar apariencia de Header & eliminar algunos console.log

--- src/app/components/show-movies/show-movies.component.ts
+++ src/app/components/show-movies/show-movies.component.ts
@@ -3,29 +3,28 @@ import { ActivatedRoute } from '@angular/router';
 import { Movie } from '@models';
 import { TMDBService } from '@services';
 
-
 @Component({
   selector: 'app-show-movies',
   templateUrl: './show-movies.component.html',
-  styleUrls: ['./show-movies.component.sass']
+  styleUrls: ['./show-movies.component.sass'],
 })
 export class ShowMoviesComponent implements OnInit {
-
   movies: Movie[] = [];
-  movieToSearch !: string;
+  movieToSearch!: string;
 
-  constructor(private route: ActivatedRoute, private tmdbService: TMDBService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private tmdbService: TMDBService
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params =>{
+    this.route.params.subscribe((params) => {
       this.movieToSearch = params['movieSearch'];
-      console.log(this.movieToSearch);
       this.updateMovie();
-    })
-
+    });
   }
 
-  updateMovie(): void{
+  updateMovie(): void {
     this.tmdbService.searchMovies(this.movieToSearch).subscribe((movies) => {
       this.movies = movies;
       console.log(this.movies);
