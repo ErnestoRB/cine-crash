1af0b04c2a445d971422403eb77ac82eef9adeec
Paulina <al217114@edu.uaa.mx>
2023-05-12 23:08:02 -0600

    tablas

--- src/app/views/candy-store/candy-store.component.ts
+++ src/app/views/candy-store/candy-store.component.ts
@@ -7,6 +7,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandyStoreComponent implements OnInit {
 
+  basePath = '../../../assets/dulces/';
+  products = [
+    [
+      {
+        desc: 'Pulparindo',
+        img: 'pulparindo.jfif'
+      },
+      {
+        desc: 'Churrumais',
+        img: 'churrumais.jpg'
+      },
+      {
+        desc: 'Doritos',
+        img: 'doritos.jfif'
+      },
+      {
+        desc: 'Takis',
+        img: 'takis.jfif'
+      }
+    ],
+    [
+      {
+        desc: 'Pringles',
+        img: 'pringles.jfif'
+      },
+      {
+        desc: 'Freskas',
+        img: 'freskas.jfif'
+      },
+      {
+        desc: 'Skittles',
+        img: 'skittles.jfif'
+      },
+      {
+        desc: 'Chokis',
+        img: 'chokis.jfif'
+      }
+    ],
+    [
+      {
+        desc: 'Oreo',
+        img: 'oreo.png'
+      },
+      {
+        desc: 'Panditas',
+        img: 'panditas.jfif'
+      },
+      {
+        desc: 'Sour Patch',
+        img: 'sour.jfif'
+      },
+      {
+        desc: 'Ring Pop',
+        img: 'ringpop.jfif'
+      }
+    ],
+    [
+      {
+        desc: 'Kit Kat',
+        img: 'kit kat.jfif'
+      },
+      {
+        desc: 'Herseys',
+        img: 'herssheys.jfif'
+      },
+      {
+        desc: 'Ruffles',
+        img: 'rufles.jfif'
+      },
+      {
+        desc: 'Magnum',
+        img: 'magnum.webp'
+      }
+    ],
+  ]
   constructor() { }
 
   ngOnInit(): void {
