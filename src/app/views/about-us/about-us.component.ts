bcf49e15909106fefd6fcf4a40502f8dbef79940
Ernesto Ramírez <ernestorb_@outlook.com>
2023-05-27 20:00:12 -0600

    Corregir Speech (evitar que se quede hablando y que siempre lea

--- src/app/views/about-us/about-us.component.ts
+++ src/app/views/about-us/about-us.component.ts
@@ -3,85 +3,76 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.sass']
+  styleUrls: ['./about-us.component.sass'],
 })
 export class AboutUsComponent implements OnInit {
-
   showDropdown: boolean = false;
   fontSize: boolean = false;
   changeFont: boolean = false;
   align: string = '';
-  isPaused: boolean = false;
+  isSpeaking: boolean = speechSynthesis?.speaking;
 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   toggleDropdown() {
     this.showDropdown = !this.showDropdown;
   }
 
   onButtonClicked(button: any) {
-    if(button.label === 'Tamaño de Texto'){
+    if (button.label === 'Tamaño de Texto') {
       this.fontSize = !this.fontSize;
-    }
-    else if(button.label === 'Tipografía'){ 
+    } else if (button.label === 'Tipografía') {
       this.changeFont = !this.changeFont;
-    }
-    else if(button.label === 'Alinear Texto'){
+    } else if (button.label === 'Alinear Texto') {
       this.changeAlign();
-    }
-    else if(button.label === 'Iniciar Lectura'){
+    } else if (button.label === 'Iniciar Lectura') {
       this.startReading();
-    }
-    else if(button.label === 'Pausar/Reanudar Lectura'){
+    } else if (button.label === 'Pausar/Reanudar Lectura') {
       this.stopStart();
-    }
-    else if(button.label === 'Terminar Lectura'){
+    } else if (button.label === 'Terminar Lectura') {
       this.endReading();
     }
   }
 
-  endReading(): void{
+  endReading(): void {
+    this.isSpeaking = false;
     speechSynthesis.cancel();
   }
 
-  changeAlign(): void{
-    if(this.align === ''){
+  changeAlign(): void {
+    if (this.align === '') {
       this.align = 'left';
-    }
-    else if(this.align === 'left'){
+    } else if (this.align === 'left') {
       this.align = 'right';
-    }
-    else if(this.align === 'right'){
+    } else if (this.align === 'right') {
       this.align = '';
-    }
-    else{
+    } else {
       this.align = '';
     }
   }
 
-  startReading(): void{
+  startReading(): void {
     const textToRead = document.getElementById('toRead')?.textContent;
-    if(textToRead && !this.isPaused){
-      const speech = new SpeechSynthesisUtterance(textToRead);
-      speech.rate = 0.7;
-      speech.lang = 'es-MX';
-      speechSynthesis.speak(speech);
+    if (textToRead && !speechSynthesis.speaking) {
+      const utterance = new SpeechSynthesisUtterance(textToRead);
+      utterance.rate = 0.7;
+      utterance.lang = 'es-MX';
+      speechSynthesis.speak(utterance);
+      this.isSpeaking = true;
     }
   }
 
-  stopStart(): void{
-    if(this.isPaused){
+  stopStart(): void {
+    if (speechSynthesis.paused) {
       speechSynthesis.resume();
-      this.isPaused = false
-    }
-    else{
+    } else {
       speechSynthesis.pause();
-      this.isPaused = true;
     }
   }
 
+  ngOnDestroy() {
+    speechSynthesis.cancel();
+  }
 }
