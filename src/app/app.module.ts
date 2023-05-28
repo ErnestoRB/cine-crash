7a89ea87faa07c19a9b56689e6dc1de83dbc3bd5
Paulina <al217114@edu.uaa.mx>
2023-05-28 15:28:46 -0600

    Hacer app una Progressive Web App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,8 @@ import {
   MovieComponent,
   SignInComponent,
 } from '@views';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -61,6 +63,12 @@ import {
     ProgressSpinnerModule,
     TableModule,
     ComponentsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [MessageService],
   bootstrap: [AppComponent],
